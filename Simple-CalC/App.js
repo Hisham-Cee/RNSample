d1a8fe80808c288e8089a47b8c00691d9436ef8c
Hisham Mohammed <hishamcee@gmail.com>
2025-07-17 16:12:27 +0530

    Along with the structure Button and input functionalities added

--- Simple-CalC/App.js
+++ Simple-CalC/App.js
@@ -11,15 +11,16 @@ export default function Calculator(){
     const n1 = parseFloat(num1);
     const n2 = parseFloat(num2);
 
-    if (isNaN(n1) || isNaN(n2))
+    if (isNaN(n1) || isNaN(n2)){
     setResult('Enter  valid numbers!');
     return;
+    }
 
     let res =0;
     if(op === '+') res = n1+n2;
     else if(op === '-') res = n1-n2;
     else if(op === 'x') res = n1*n2;
-    else if(op === '/') res = n2 !=0 ? n1/n2 : '0 is not divisible by the number';
+    else if(op === '/') res = n2 !=0 ? n1/n2 : 'cannot divide by 0';
     setResult(res.toString());
  }
 
@@ -32,34 +33,34 @@ export default function Calculator(){
         <View style={styles.inputText}>
           <TextInput 
             value={num1}
-            keyboardType="numeric"
+            keyboardType="number-pad"
             onChangeText={setNum1}/>
         </View>
         <View style={styles.inputText}>
           <TextInput
             value={num2}
-            keyboardType="numeric"
+            keyboardType="number-pad"
             onChangeText={setNum2} />
         </View>
       </View>
       <View style={styles.buttonContainer}>
         <View style={styles.buttons}>
-          <Pressable style={styles.button} onPress={onPressHandler}>
+          <Pressable style={styles.button} onPress={() => onPressHandler('+')}>
             <Text style={styles.buttonText}>+</Text>
           </Pressable>
         </View>
         <View style={styles.buttons}>
-          <Pressable style={styles.button} onPress={onPressHandler}>
+          <Pressable style={styles.button} onPress={() => onPressHandler('-')}>
             <Text style={styles.buttonText}>-</Text>
           </Pressable>
         </View>
         <View style={styles.buttons}>
-          <Pressable style={styles.button} onPress={onPressHandler}>
+          <Pressable style={styles.button} onPress={() => onPressHandler('x')}>
             <Text style={styles.buttonText}>x</Text>
           </Pressable>
         </View>
         <View style={styles.buttons}>
-          <Pressable style={styles.button} onPress={onPressHandler}>
+          <Pressable style={styles.button} onPress={() => onPressHandler('/')}>
             <Text style={styles.buttonText}>/</Text>
           </Pressable>
         </View>
@@ -83,7 +84,15 @@ const styles = StyleSheet.create({
     fontSize: 24,
   },
   inputContainer:{},
-  inputText:{},
+  inputText:{
+    width: '80%',
+    borderColor: '#000',
+    borderWidth: 1,
+    borderRadius: 5,
+    padding: 10,
+    marginVertical: 10,
+    color: '#fff',
+  },
   buttonContainer:{
     flexDirection: 'row',
     gap:20
